a468950b3f20c4616dffbc3b3a73c0e8d28f7ef0
David Lowry-Duda <davidlowryduda@davidlowryduda.com>
2018-05-24 01:14:04 -0700

    Add parser tests

--- tests.py
+++ tests.py
@@ -2,8 +2,9 @@
 import unittest
 import contextlib
 from io import StringIO
+from optparse import OptionParser
 
-from tld import *
+from tld import TaskDict, build_parser
 
 class Basic_Task_Structure(unittest.TestCase):
     def setUp(self):
@@ -28,6 +29,18 @@ class Basic_Task_Structure(unittest.TestCase):
         with contextlib.redirect_stdout(tmp_stdout):
             self.td.print_list()
         self.assertEqual(tmp_stdout.getvalue(), goal)
+
+class Basic_Parser_Operation(unittest.TestCase):
+    def test_add(self):
+        input_args = ["-a", "test task 1"]
+        (options, args) = build_parser().parse_args(input_args)
+        self.assertTrue(options.add == True)
+        self.assertTrue(args[0] == "test task 1")
+
+    def test_list(self):
+        input_args = ["-l", "othertasks"]
+        (options, args) = build_parser().parse_args(input_args)
+        self.assertTrue(options.name == "othertasks")
 
 
 if __name__ == "__main__":
